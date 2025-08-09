adff0209f1e2853a4cb1d2147d76abbfc64731da
Sizzlesina <www.sins604@gmail.com>
2025-08-09 21:56:12 +0330

    last changes

--- src/app/(home)/blog/category/news/page.jsx
+++ src/app/(home)/blog/category/news/page.jsx
@@ -1,4 +1,4 @@
-import  { createArticleAPI } from "@/app/actions/article";
+import { createArticleAPI } from "@/app/actions/article";
 import React from "react";
 import { faker } from "@faker-js/faker";
 import Image from "next/image";
@@ -14,9 +14,9 @@ export default function NewsPage() {
   );
 
   return (
-    <div className="flex flex-col gap-2 mt-15">
+    <div className='flex flex-col gap-2 mt-15'>
       <BlogPageHeader />
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 ">
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 '>
         {FilteredArticles.map((article) => {
           return (
             <div className={`flex flex-col gap-3 `}>
@@ -28,23 +28,22 @@ export default function NewsPage() {
                 width={200}
                 height={200}
               />
-              <div className="flex flex-col gap-3">
+              <div className='flex flex-col gap-3'>
                 <Link
                   href={`/blog/${article.id}`}
-                  className="text-xl font-semibold"
-                >
+                  className='text-xl font-semibold'>
                   {article.title}
                 </Link>
-                <p className="fontlight line-clamp-2">{article.description}</p>
-                <div className="flex gap-2 items-center">
+                <p className='fontlight line-clamp-2'>{article.description}</p>
+                <div className='flex gap-2 items-center'>
                   <Image
-                    className="rounded-full"
+                    className='rounded-full'
                     src={article.authorImage}
                     alt={article.authorName}
                     width={35}
                     height={35}
                   />
-                  <p className="fontlight font-semibold text-sm">
+                  <p className='fontlight font-semibold text-sm'>
                     {article.publishedDate.toLocaleDateString("en-US", {
                       year: "numeric",
                       month: "long",
